b7296c0c785ce54f67cc02d50579bdbadb8488a8
mehdi <mehdi>
2024-09-21 19:25:58 +0100

    fef

---
+++ Makefile
@@ -0,0 +1,38 @@
+WP_DATA = /home/data/wordpress
+MDB_DATA = /home/data/mariadb 
+
+all: up
+
+up: build
+	@mkdir -p $(WP_DATA)
+	@mkdir -p $(MDB_DATA)
+	docker-compose -f ./src/docker-compose.yml up -d
+
+down:
+	docker-compose -f ./src/docker-compose.yml down
+
+stop:
+	docker-compose -f ./src/docker-compose.yml stop
+
+start:
+	docker-compose -f ./src/docker-compose.yml start
+
+build:
+	docker-compose -f ./src/docker-compose.yml build
+
+rmi:
+	docker rmi -f $$(docker images -qa)
+
+clean:
+	@docker stop $$(docker ps -qa) || true
+	@docker rm $$(docker ps -qa) || true
+	@docker rmi -f $$(docker images -qa) || true
+	@docker volume rm $$(docker volume ls -q) || true
+	@docker network rm $$(docker network ls -q) || true
+	@rm -rf $(WP_DATA) || true
+	@rm -rf $(MDB_DATA) || true
+
+re: clean up
+
+fclean: clean
+	@docker system prune -a --volumes -f
---
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www/wordpress
+chmod -R 755 /var/www/wordpress/
+chown -R www-data:www-data /var/www/wordpress
+
+ping_mdb() {
+    nc -zv mariadb 3306 > /dev/null
+    return $?
+}
+var=0
+while [var -lt 10]; do
+    ping_mdb
+    if [ $? -eq 0 ]; then
+        echo "***MARIADB IS RUNNING***"
+        break
+    else
+        echo "***PENDINGGG***"
+        sleep 1
+    fi
+done
+
+
+if [ var -gt 10 ]; then
+    echo "***MARIADB IS NOT RESPONDING***"
+fi
+
+wp core download --allow-root
+wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
+
+
+sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir -p /run/php
+/usr/sbin/php-fpm7.4 -F
